20a5294027df437b2f8f11f494422b4034657674
Niracha <61010588@kmitl.ac.th>
2020-04-28 15:49:36 +0700

    upload image

--- buffet2gether_home/lib/services/auth.dart
+++ buffet2gether_home/lib/services/auth.dart
@@ -33,7 +33,7 @@ class AuthService {
 
 //register
   Future registerWithEmailAndPassword(String email, String password,
-      String username, String gender, DateTime dateOfBirth) async {
+      String profilePic,String username, String gender, DateTime dateOfBirth) async {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
@@ -41,7 +41,8 @@ class AuthService {
       //creat a new doc for the user with uid
       await DatabaseService(uid: user.uid).updateUserData(
         ///////////////////default value
-          'https://firebasestorage.googleapis.com/v0/b/buffet2gether.appspot.com/o/profile_pictures%2Fdefault.png?alt=media&token=824df76d-dc2b-4a05-8d08-4414dc03750e',
+          profilePic,
+          //'https://firebasestorage.googleapis.com/v0/b/buffet2gether.appspot.com/o/profile_pictures%2Fdefault.png?alt=media&token=824df76d-dc2b-4a05-8d08-4414dc03750e',
           username,
           gender,
           dateOfBirth,
@@ -53,7 +54,7 @@ class AuthService {
           true,
           true,
           true,
-          false,);
+          false);
       await DatabaseService().updateTableData(null, null, user.uid) ;
       return _userFromFirebaseUser(user);
     } catch (e) {
